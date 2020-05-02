f11096c4962faed978ab16270988749738f38d3c
UpperCod <mtrujillo@outlook.cl>
2020-05-02 18:31:16 -0400

    build doc

---
+++ docs/example-test.js
@@ -0,0 +1,2 @@
+before(()=>[...Array(1e3).keys()]),test("find 100",e=>{e.find(e=>100==e)}),test("find 200",e=>{e.find(e=>200==e)}),test("find 400",e=>{e.find(e=>400==e)}),after(e=>{console.log(e)});
+//# sourceMappingURL=example-test.js.map
